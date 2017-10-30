abda5010ef50e53463c42fd6946999debe77c1b5
sophiearana <arana.sophie@gmail.com>
2017-10-30 21:27:55 +0100

    new RT plot

--- Limesurvey/results/preps_analyse_pretest.R
+++ Limesurvey/results/preps_analyse_pretest.R
@@ -1,5 +1,8 @@
 library(plotly)
 library(reshape)
+library("RColorBrewer")
+pastel_colors = brewer.pal(8, "Pastel1")
+set_colors = brewer.pal(8, "Set1")
 #load stimulus file
 stimuli         <- read.csv(file="stimuliA.csv",head=TRUE,sep=",",na.strings = "NAN", stringsAsFactors = FALSE)
 stimuli$fullID  <- as.character(interaction(stimuli[,c(13,3)],sep = ""))
@@ -30,30 +33,29 @@ count_correct = apply(df.responses[,subset_unambiguous], 1, function(x) sum(x ==
 count_correct/length(subset_unambiguous)
 #Biases: Reaction Times total and per attachment
 subset_time   <- grep("^[VN]A[0-9]*Time$",names(df.responses),value = TRUE)
-rts_persubj   <- melt(t(df.responses[,subset_time]))
-colnames(rts_persubj) <- c("itemcode","subject","rt")
-rts_persubj$attachment <- rep("Noun",nrow(rts_persubj))
-rts_persubj[grep("^VA",rts_persubj$itemcode),4] <- rep("Verb",nrow(rts_persubj)/2)
+df.rts        <- df.responses[,subset_time]
+y1            <- rowMeans(df.rts[,grep("^VA",names(df.rts))])
+y2            <- rowMeans(df.rts[,grep("^NA",names(df.rts))])
+y3            <- rowMeans(df.rts)
 
 p <- plot_ly(type = 'box') %>%
   add_boxplot(y = y1, jitter = 0.3, pointpos = -1.8, boxpoints = 'all',
-              marker = list(color = 'rgb(7,40,89)'),
-              line = list(color = 'rgb(7,40,89)'),
-              name = "Verb attachment") %>%
-  add_boxplot(y = y1, jitter = 0.3, pointpos = -1.8, boxpoints = 'all',
-              marker = list(color = 'rgb(7,40,89)'),
-              line = list(color = 'rgb(7,40,89)'),
+              marker = list(color = pastel_colors[1]),
+              line = list(color = pastel_colors[1]),
+              name = "Verb attachment",
+              hoverinfo= row.names(df.rts)) %>%
+  add_boxplot(y = y2, jitter = 0.3, pointpos = -1.8, boxpoints = 'all',
+              marker = list(color = pastel_colors[2]),
+              line = list(color = pastel_colors[2]),
               name = "Noun attachment") %>%
-  add_boxplot(y = y1, jitter = 0.3, pointpos = -1.8, boxpoints = 'all',
-              marker = list(color = 'rgb(7,40,89)'),
-              line = list(color = 'rgb(7,40,89)'),
-              name = "All trials") %>%
-  layout(title = "Reaction times across items & subjects (data points are subject averages")
+  add_boxplot(y = y3, name = "All trials", boxpoints = 'suspectedoutliers',
+              marker = list(color = pastel_colors[3],
+                            outliercolor = set_colors[1],
+                            line = list(outliercolor = set_colors[1],
+                                        outlierwidth = 2)),
+              line = list(color = pastel_colors[3])) %>%
+  layout(title = "Reaction times across items & subjects (data points are subject averages)",
+         yaxis=list(title="RTs (seconds)"))
 
 
-subset_attach <- grep("^NA[0-9]*$",names(df.responses),value = TRUE)
-idx           <- match(subset_attach,names(df.responses))
-df.Nattach    <- df.responses[,subset_attach]
-subset_attach <- grep("^VA[0-9]*$",names(df.responses),value = TRUE)
-idx           <- match(subset_attach,names(df.responses))
-df.Vattach    <- df.responses[,subset_attach]
+
