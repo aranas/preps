d3de780dfc2bc5a0105cff94829c124f38d7efe9
Sophie Arana <arana.sophie@gmail.com>
2018-01-08 17:00:43 +0100

    add paths

--- MEG/preps_decoding_pipeline.m
+++ MEG/preps_decoding_pipeline.m
@@ -5,7 +5,8 @@
 %               and n features.
 % labels    = mx1 column vector containing the labels for each trial/observation.
 % 
-
+addpath('/home/language/sopara/Prepositionalphrases/Scripts_tom/GNB')
+addpath('/home/language/sopara/Prepositionalphrases/preps/MEG/')
 %% Script variables (to be specified before running script)
 if ~exist('pathname', 'var')
     error('a pathname needs to be provided');
@@ -19,6 +20,7 @@ if ~exist('dofolds','var'),         dofolds         = 1; end
 if ~exist('doholdout','var'),       doholdout       = 0; end
 if ~exist('donaivebayes','var'),    donaivebayes    = 1; end
 if ~exist('doridge','var'),         doridge         = 0; end
+if ~exist('dozscore','var'),        dozscore        = 1; end % ridge regression script does zscoring on its own.
 if ~exist('dopermutelabels','var'), dopermutelabels = 0; end
 
 timestep    = 'all';    %default: string 'all' to use all features, otherwise specify stepsize in sensors*samples.
@@ -27,7 +29,7 @@ nleaveout   = 2;        %if smaller than 1 ( but positive) will be interpreted a
 
 folds      = 10;       % select number of folds for cross-validation
 
-repetitions = 5;        % default no repetitions
+repetitions = 0;        % default no repetitions
 
 %nfeatures   = 10000    % set how many features of gaussian naive bayes to
 %use for testing
@@ -79,6 +81,13 @@ for nrep = 1:(1+repetitions)
                 data_train  = datap(test_id.training(nfold),:);
                 label_test  = labels(test_id.test(nfold));
                 label_train = labels(test_id.training(nfold));
+                
+                if dozscore
+                   
+                   [data_train, Mu, Sigma] = zscore(data_train);
+                    data_test              = bsxfun(@rdivide, bsxfun(@minus, data_test, Mu),Sigma);
+                
+                end
                 
                 %%% TRAIN & PREDICT %%%
                 
