9d7f7796113ab28a5bd5b5cec82d11cd68f0c527
Sophie Arana <arana.sophie@gmail.com>
2018-01-09 18:03:38 +0100

    update file

--- MEG/preps_decoding.m
+++ MEG/preps_decoding.m
@@ -1,7 +1,8 @@
 addpath(genpath('/home/language/sopara/Matlab/fieldtrip/external/dmlt'))
 addpath('/home/language/sopara/Prepositionalphrases/preps/MEG')
+addpath('/home/language/sopara/Prepositionalphrases/Scripts_tom/GNB')
 
-load('/project/3011210.01/MEG/pilot_VAvsNAvsFill.mat')
+load('/project/3011210.01/MEG/pilot_DetvsNoun.mat')
 
 
 cfg             = [];
@@ -10,33 +11,23 @@ avg_data        = ft_timelockanalysis(cfg,data);
 
 
 cfg             = [];
-cfg.method      = 'crossvalidate_sa';
-cfg.mva         = {dml.standardizer dml.naive}; %default
+cfg.method      = 'crossvalidate';
+cfg.mva         = {dml.standardizer dml.naive}; % 'preps_naivebayes'%;
 cfg.statistic   = {'accuracy'};
-cfg.nfolds      = 20;
+cfg.type        = 'nfold';
+cfg.nfolds      = 5;
 cfg.resample    = true;% default: false;
+cfg.latency     = [0.1 0.4];
 cfg.design      = labels;
 %cfg.checksize   = 4000000; %allow to keep cv field in cfg (although large)
 
 %loop over time
 acc             = [];
-for i = 0.1:0.1:1.5
+for i = -0.5:0.1:1
 cfg.latency     = [i-0.1 i];
 class           = ft_timelockstatistics(cfg,avg_data);
 acc             = [acc class.statistic.accuracy];
 acc
 end
-
-cfg             = [];
-cfg.method      = 'crossvalidate_sa';
-cfg.mva         = 'ridgeregression_sa';
-cfg.statistic   = {'accuracy','binomial'};
-cfg.nfolds      = 5;
-cfg.numlambdas  = 10;
-cfg.resample    = true;% default: false;
-cfg.latency     = [0.1 0.4];
-cfg.design      = labels;
-%cfg.checksize   = inf; %allow to keep cv field in cfg (although large)
-class           = ft_timelockstatistics(cfg,avg_data)
 
 
