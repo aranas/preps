568897aa55d2d71be7fd00ba8d244efef8ff0bc5
Sophie Arana <arana.sophie@gmail.com>
2018-01-24 16:04:10 +0100

    added random data creation, pca, fixed permutation of labels and added cfg options

--- MEG/preps_decoding.m
+++ MEG/preps_decoding.m
@@ -2,10 +2,24 @@ addpath(genpath('/home/language/sopara/Matlab/fieldtrip/external/dmlt'))
 addpath /home/common/matlab/fieldtrip/qsub/
 addpath('/home/language/sopara/Prepositionalphrases/preps/MEG')
 addpath('/home/language/sopara/Prepositionalphrases/Scripts_tom/GNB')
-addpath(genpath('home/language/sopara/Matlab/distributionPlot'))
+addpath(genpath('/home/language/sopara/Matlab/distributionPlot'))
 
 load('/project/3011210.01/MEG/p2_NounvsAdj.mat')
 
+% %emulate random data
+% for l = 1:size(data.trial,2)
+%     data_rand{l} = randn(270,1800)*5^-13;
+% end
+% 
+% data.trial = data_rand;
+
+%decompose matrix and only keep 60 components
+% cfg              = [];
+% cfg.demean       = 'yes';
+% cfg.scale        = 'no';
+% cfg.method       = 'pca';
+% cfg.numcomponent = 60;
+% data         = ft_componentanalysis(cfg, data);
 
 cfg             = [];
 cfg.keeptrials  = 'yes';
@@ -13,22 +27,26 @@ avg_data        = ft_timelockanalysis(cfg,data);
 
 indx_1          = find(labels==1);
 indx_2          = find(labels==2);
+indx_1          = indx_1(randperm(size(indx_1,1)));
+indx_2          = indx_2(randperm(size(indx_2,1)));
+
 labels_perm     = labels;
-labels_perm(indx_1(1:(length(indx1)/2))) = 2;
-labels_perm(indx_2(1:(length(indx2)/2))) = 1;
+labels_perm(indx_1(1:(length(indx_1)/2))) = 2;
+labels_perm(indx_2(1:(length(indx_2)/2))) = 1;
 
 
 
 cfg             = [];
 cfg.method      = 'crossvalidate';
-cfg.mva         = 'preps_naivebayes';%{dml.standardizer dml.naive}; % 'preps_naivebayes';%;
+cfg.mva         = 'preps_naivebayes';%{dml.standardizer dml.svm}; % 'preps_naivebayes';%;
 cfg.statistic   = {'accuracy'};
 cfg.type        = 'nfold';
-cfg.nfolds      = 5;
+cfg.nfolds      = 20;
 %cfg.resample    = 1;% default: false; resampling for 'loo' retuns zero
 %vector for testfolds
 cfg.design      = labels_perm;
 cfg.poolsigma   = 0;
+cfg.numfeat     = 500;
 %cfg.checksize   = 4000000; %allow to keep cv field in cfg (although large)
 
 %loop over time & repeats
@@ -39,14 +57,14 @@ jobid    = cell(length(t),repeat);
 
 acc      = zeros(length(t),repeat);
 tic
-for i = 1:length(t)
-cfgt             = [];
-cfgt.latency     = [t(i)-0.1 t(i)];
-datatmp        = ft_selectdata(cfgt,avg_data);
-
+for  i = 1:length(t)
+      cfgt             = [];
+      cfgt.latency     = [t(i)-0.1 t(i)];
+      datatmp        = ft_selectdata(cfgt,avg_data);
+      
   for rep = 1:repeat
     %class           = ft_timelockstatistics(cfg,datatmp);
-    jobid{i,rep}    = qsubfeval('ft_timelockstatistics',cfg,datatmp,'memreq',(1024^3)*2,'timreq',600,'batchid',strcat('naive_',int2str(i),'_',int2str(rep)));
+    jobid{i,rep}    = qsubfeval('ft_timelockstatistics',cfg,datatmp,'memreq',1024^3,'timreq',100,'batchid',strcat('naive_',int2str(i),'_',int2str(rep)));
   end
   %collect results for each time slice to avoid memory problems
   for rep2 = 1:repeat
@@ -55,6 +73,6 @@ datatmp        = ft_selectdata(cfgt,avg_data);
   end
 end
 toc
-acc_perm_5f = acc;
+acc_perm_500feat = acc;
 
 
