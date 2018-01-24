4a6a95fd59961e5dafcc9beacfc033ea767fab2a
Sophie Arana <arana.sophie@gmail.com>
2018-01-24 16:03:11 +0100

    define number of features to be selected according to interclass mu, option to be specified in cfg.

--- MEG/preps_naivebayes.m
+++ MEG/preps_naivebayes.m
@@ -3,6 +3,10 @@ function [beta_hat,probs,out] = preps_naivebayes(cfg,trainX,testX,trainY)
 % [trainX, Mu, Sigma]     = zscore(trainX);
 % testX                   = bsxfun(@rdivide, bsxfun(@minus, testX, Mu),Sigma);
 
+ if ~isfield(cfg,'numfeat')
+     cfg.numfeat = size(trainX,2);
+ end
+
 Mu = nanmean(trainX);
 Sigma = nanstd(trainX);
 
@@ -22,8 +26,8 @@ end
 beta_hat               = nbayes_train(trainX, trainY', cfg.poolsigma); %third argument yes/no pool sigma?
 
 % use top N features
-beta_hat.sortedFeats   = sortGNBfeaturesByMuDifference(beta_hat);
-selectedFeats          = beta_hat.sortedFeats(:);
+beta_hat.sortedFeats   = sortGNBfeaturesByMuDifference(beta_hat,1); %2nd argument useSigma to normalize MuDifference yes/no
+selectedFeats          = beta_hat.sortedFeats(1:cfg.numfeat);
 
 probs                  = nbayes_apply(testX, beta_hat,selectedFeats);
 out                    = [Mu Sigma];
