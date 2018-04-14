e41cdd6a1a79019a1b3c44fe398d24d6fd3d5a98
Sophie Arana <arana.sophie@gmail.com>
2018-04-14 08:42:32 +0200

    now also works for uneven number of samples and saves part of speech label for both correct and incorrect predictions

--- MEG/eval_euclideandistance.m
+++ MEG/eval_euclideandistance.m
@@ -1,16 +1,21 @@
-function [acc, dist] = eval_euclideandistance(cfg,obj)
+function [acc, dist, correct_pos, incorrect_pos] = eval_euclideandistance(cfg,obj)
 
 success = 0;
 fail    = 0;
 dist    = cell(length(cfg.nfolds),1);
+incorrect_pos = {};
+correct_pos    = {};
 for f = 1:cfg.nfolds
     
     y_hat                = obj.result{f};
     y_test               = obj.design{f};
     
     [num,~]              = size(y_test);
-    y_test1              = y_test(1:num / 2 , :)' ;
-    y_test2              = y_test(num / 2 + 1:end , :)' ;
+    if mod(num,2)~= 0
+        num = num-1;
+    end
+    y_test1              = y_test(1:(num / 2) , :)' ;
+    y_test2              = y_test(num / 2 + 1:num , :)' ;
     y_test1              = y_test1(:) ;
     y_test2              = y_test2(:) ;
     
@@ -18,7 +23,7 @@ for f = 1:cfg.nfolds
     %euclidean distance between vectors belonging together should be smaller
     %than distance between vectors not belonging together.
     y_hat1               = y_hat(1:num / 2 , :)' ;
-    y_hat2               = y_hat(num/ 2 + 1:end , :)' ;
+    y_hat2               = y_hat(num/ 2 + 1:num , :)' ;
     % number of trials is odd, therfore folds are odd, therefore test1
     % longer than test2 - problematic?
     y_hat1               = y_hat1(:) ;
@@ -30,11 +35,14 @@ for f = 1:cfg.nfolds
     
     if correct == 1
         success          = success + 1;
+        correct_pos{f}   = obj.pos{f};
     else
         fail             = fail + 1;
+        incorrect_pos{f}   = obj.pos{f};
     end
     acc = success / (success + fail);
     
 end
-
+correct_pos = correct_pos(~cellfun('isempty',correct_pos));
+incorrect_pos = incorrect_pos(~cellfun('isempty',incorrect_pos));
 end
