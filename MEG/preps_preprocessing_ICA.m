28945be52638f5b38e8d6135405a7cb52978335f
Sophie Arana <arana.sophie@gmail.com>
2018-05-30 11:14:44 +0200

    filter before ica, update header with variable settings

--- MEG/preps_preprocessing_ICA.m
+++ MEG/preps_preprocessing_ICA.m
@@ -1,38 +1,52 @@
 clear all
 clc
 
-file                   = dir(strcat('/project/3011210.01/raw/sub-',subj,'/ses-meg01/meg/'));
+%% Specify variables
+if ~exist('subj',        'var'), subj         = 'pilot-005';  end
+if ~exist('trigger',     'var'), trigger      = [140,240,40,31:39,110:119,120:129,210:219,220:229];  end
+if ~exist('trigger_last','var'), trigger_last = [119,129,219,229];  end
+if ~exist('root_dir',    'var'), root_dir     = '/project/3011210.01/';  end
+
+%% epoch data from raw files
+file                    = dir(strcat('/project/3011210.01/raw/',subj,'/ses-meg01/meg/'));
 
 cfg                     = [];
-cfg.dataset             = strcat('/project/3011210.01/raw/sub-',subj,'/ses-meg01/meg/',file(3).name);
+cfg.dataset             = strcat(root_dir,'raw/',subj,'/ses-meg01/meg/',file(3).name);
+cfg.logfile             = strcat(root_dir,'logfiles/',subj, '_log.txt');
 cfg.trialdef.prestim    = 0.2;
-cfg.trialdef.poststim   = 2.7;
+cfg.trialdef.poststim   = 2.8;
 cfg.trialdef.eventtype  = 'UPPT001';
-%cfg.trialdef.eventvalue = [131:138,110:118,120:128,210:218,220:228];
+cfg.trialdef.eventvalue = trigger;
+cfg.trialfun            = 'ft_trialfun_preps';
 new_cfg                 = ft_definetrial(cfg);
 
 new_cfg.channel         = {'MEG', 'EEG'};
 new_cfg.continuous      = 'yes';
-new_cfg.demean          = 'yes';
-new_cfg.dftfilter       = 'yes';    %get rid of line noise
+new_cfg.lpfilter        = 'yes';
+new_cfg.lpfreq          = 30;
+new_cfg.lpfilttype      = 'firws';
+new_cfg.hpfilter        = 'yes';
+new_cfg.hpfreq          = 1;
+new_cfg.hpfilttype      = 'firws';
+new_cfg.usefftfilt      = 'yes';
 new_cfg.padding         = 10;
 data                    = ft_preprocessing(new_cfg);
 
-
-ind_lastword            = find(ismember(data.trialinfo,[119,129,219,229]));
-toi                     = repmat([data.time{1}(1) 0.7],length(data.trial),1);
+% redefine to keep longer time windows only for sentence-final words
+ind_lastword            = find(ismember(data.trialinfo,trigger_last));
+toi                     = repmat([data.time{1}(1) 0.8],length(data.trial),1);
 toi(ind_lastword,:)     = repmat([data.time{1}(1) data.time{1}(end)],length(ind_lastword),1);
 
 cfg                     = [];
 cfg.toilim              = toi;
 cfg.trials              = 'all';
-data_new                = ft_redefinetrial(cfg,data);
+data                    = ft_redefinetrial(cfg,data);
 
-% Muscle artifacts
+% Inspect muscle artifacts to get feel for how noisy data is
 cfg                     = [];
 cfg.preproc.hpfilter    = 'yes';
 cfg.preproc.hpfreq      = 100;
-tmp_data_muscle         = ft_rejectvisual(cfg, data_new);
+tmp_data_muscle         = ft_rejectvisual(cfg, data);
 
 
 %% compute ICA on data to remove ECG/EOG artifacts
@@ -73,19 +87,18 @@ r = corr(cell2mat(comp.trial)', EEG');
 
 %% STEP 3.2
 % inspect ICA component and marks those related to ECG/EOG artifacts as bad
-cfg = [];
+cfg           = [];
 cfg.component = [1:30];       % specify the component(s) that should be plotted
 cfg.layout    = 'CTF275.lay'; % specify the layout file that should be used for plotting
 cfg.comment   = 'no';
 ft_topoplotIC(cfg, compds)
 
-fh = figure();
-cfg = [];
-%cfg.channel = [1:30]; % components to be plotted
-cfg.channel     = unique(j((end-2):end, :));
-cfg.compscale   = 'local';
-cfg.viewmode = 'component';
-cfg.layout = 'CTF275.lay'; % specify the layout file that should be used for plotting
+fh            = figure();
+cfg           = [];
+cfg.channel   = unique(j((end-2):end, :));
+cfg.compscale = 'local';
+cfg.viewmode  = 'component';
+cfg.layout    = 'CTF275.lay'; % specify the layout file that should be used for plotting
 ft_databrowser(cfg, compds)
 
 waitfor(fh) % wait until we have closed the window for entring bad components
@@ -109,14 +122,25 @@ while promptUser
 end
 %% STEP 3.3
 % remove components related to ECG and EOG artifacts and backproject the data
-cfg = [];
-cfg.channel = 'MEG';
-cfg.component = badcomp; % to be removed component(s)
-data = ft_rejectcomponent(cfg, comp, data);
+cfg                 = [];
+cfg.channel         = 'MEG';
+cfg.component       = badcomp; % to be removed component(s)
+data                = ft_rejectcomponent(cfg, comp, data);
+
+cfg                 = [];
+cfg.resamplefs      = 300;
+cfg.detrend         = 'no';  % not good for evoked data
+cfg.demean          = 'no';
+cfg.trials          = 'all';
+data                = ft_resampledata(cfg, data);
 
 % Remove non-MEG channels, because ft_rejectcomponent adds those again
-cfg         = [];
-cfg.channel = 'MEG';
-data        = ft_preprocessing(cfg, data);
+cfg                 = [];
+cfg.channel         = 'MEG';
+cfg.detrend         = 'no';
+cfg.demean          = 'yes';
+cfg.baselinewindow  = [-inf 0];
+data                = ft_preprocessing(cfg, data);
 
-save('/project/3011210.01/MEG/sub001_data_clean_alltrials','data','compds','badcomp','-v7.3')
+
+save(strcat(root_dir,'MEG/',subj,'_dataclean'),'data','compds','badcomp','-v7.3')
