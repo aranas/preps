4881179be7e1b0e3d9b8085af246e13850cde6fa
Sophie Arana <arana.sophie@gmail.com>
2018-09-20 16:58:00 +0200

    evaluate best lambda parameter based on corr/mse switch

--- MEG/ridgeregression_sa.m
+++ MEG/ridgeregression_sa.m
@@ -4,6 +4,8 @@ cfg.nfolds = 10;
 if ~isfield(cfg, 'numlambdas'), cfg.numlambdas = 10;  end
 if isfield(cfg, 'lambda'), lambdas = cfg.lambda; end
 if ~isfield(cfg, 'constant'), cfg.constant = 0; end
+if ~isfield(cfg, 'lambdaeval'),cfg.lambdaeval = 'corr'; end
+if ~isfield(cfg,'plotlambdas'),cfg.plotlambdas = 0;     end
 
 [m,n]                = size(y_train);
 %add constant & zscore
@@ -19,26 +21,43 @@ end
 varx                 = x_train * x_train';
 
 % train ridge regression with several lambda values using cross-validation
-if exist('lambdas','var') && size(lambdas,1)<2
+if ~isempty(lambdas) && exist('lambdas','var') && size(lambdas,1)<2
     lambda_hat       = repmat(lambdas,n,1);
 else
-    if exist('lambdas','var')
-        [R, lambdas]         = get_R_and_lambda(x_train,varx, y_train, cfg.nfolds, length(lambdas),lambdas);
+    if exist('lambdas','var') && ~isempty(lambdas)
+        [R, lambdas, mse, norme, normb, nrmsd]         = get_R_and_lambda(x_train,varx, y_train, cfg.nfolds, length(lambdas),lambdas);
     else
-        [R, lambdas]         = get_R_and_lambda(x_train,varx,y_train,cfg.nfolds, cfg.numlambdas); %k =  folds for crossval; 10 lambda values
+        [R, lambdas, mse, norme, normb, nrmsd]         = get_R_and_lambda(x_train,varx,y_train,cfg.nfolds, cfg.numlambdas); %k =  folds for crossval; 10 lambda values
     end
-    % force to use regularization:
-    R = R(:,2:end);
-    lambdas = lambdas(2:end);
+%     if cfg.plotlambdas
+%         subplot(2,4,1)
+%         plot(log(lambdas),mse)
+%         title('mean squared error')
+%         subplot(2,4,2)
+%         plot(log(lambdas),R')
+%         title('correlation')
+%         subplot(2,4,3)
+%         plot(log(lambdas),nrmsd)
+%         title('normalized root mean squared error')
+%         subplot(2,4,4)
+%         loglog(norme,normb)
+%         %plot L-curve
+%         title('L-curve')
+%         suptitle('in-sample')
+%     end
+%     
     % select optimal lambda values
     r_hat                = NaN(n, 1);
     lambda_hat           = NaN(n, 1);
     
     for feat   = 1 : n
-        
+        switch cfg.lambdaeval
+            case 'mse'
+        [r_hat(feat), I] = min(mse(1:end,feat));
+            otherwise
         [r_hat(feat), I] = max(R(feat, 1:end));
-        lambda_hat(feat) = lambdas(I);
-        
+        end
+        lambda_hat(feat) = lambdas(I);       
     end
 end
 % Compute beta values with optimal Lambda
