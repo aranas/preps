d65300818849bcebcf388565470aa3a7954f29c8
Sophie Arana <arana.sophie@gmail.com>
2018-10-03 16:58:35 +0200

    adjust file to work within preps_decoding and ft_statistics crossvalidate

--- MEG/tudacv_sa.m
+++ MEG/tudacv_sa.m
@@ -1,4 +1,4 @@
-function [R2,pval,surrogates,c] = tudacv_sa(X,Y,T,cfg,Gamma)
+function [models, results, design] = tudacv_sa(X,Y,T,cfg,Gamma)
 % Adapted from Vidaurre's toolbox https://github.com/OHBA-analysis/HMM-MAR/wiki/User-Guide
 % changed to work with models other than regression
 
@@ -9,22 +9,22 @@ function [R2,pval,surrogates,c] = tudacv_sa(X,Y,T,cfg,Gamma)
 %   Vidaurre et al (2017). ***TO COMPLETE***
 %   (section 'Modelling between-trial temporal differences improves decoding
 %   performance')
-% NOTE: Specificying Nfeatures will lead to a circular assessment 
+% NOTE: Specificying Nfeatures will lead to a circular assessment
 % (overfitting of R2), so this is discouraged
-% 
+%
 % INPUT
 % X: Brain data, (time by regions)
 % Y: Stimulus, (time by q); q is no. of stimulus features
 % T: Length of series
-% options: structure with the training options - see documentation in 
+% options: structure with the training options - see documentation in
 %                       https://github.com/OHBA-analysis/HMM-MAR/wiki
 %  Apart from the options specified for tudatrain, specific to tudacv are:
-%  - options.mode, referring to how to do cross-validation. 
-%                  . if 1, CV is done time point by time point, across CV folds, 
-%                   to produce a (time by 1) accuracy vector. 
+%  - options.mode, referring to how to do cross-validation.
+%                  . if 1, CV is done time point by time point, across CV folds,
+%                   to produce a (time by 1) accuracy vector.
 %                  . if 2, CV is performed across time points, so we would get
 %                   a (time by time) accuracy "generalisation" matrix.
-%  - options.CVmethod, cross-validation with TUDA has a fundamental issue: 
+%  - options.CVmethod, cross-validation with TUDA has a fundamental issue:
 %                  that the state time courses are estimated
 %                  using both the data and the stimuli; then in the
 %                  held-out data there is no way to know which state is
@@ -33,7 +33,7 @@ function [R2,pval,surrogates,c] = tudacv_sa(X,Y,T,cfg,Gamma)
 %                  This options establishes how to compute the model time
 %                  courses in the held-out data
 %                  . if 2, the state time courses are estimated using
-%                  just data and linear regression, i.e. we try to predict 
+%                  just data and linear regression, i.e. we try to predict
 %                  the state time courses in held-out trials using the data
 %                  . if 1, then it uses the state active at each time point is taken
 %                  to be the one that is most active in training at each
@@ -44,7 +44,7 @@ function [R2,pval,surrogates,c] = tudacv_sa(X,Y,T,cfg,Gamma)
 %                  . if -2, then the actual held-out state time courses are used.
 %  - options.NCV, containing the number of cross-validation folds (default 10)
 %  - options.lambda, regularisation penalty for the decoding
-%  - options.lossfunc, loss function to compute the cross-validated error, 
+%  - options.lossfunc, loss function to compute the cross-validated error,
 %                  default is 'quadratic', in which case R2 corresponds to
 %                  explained variance. Other options are: 'absolute', 'huber'
 %  - options.Nperm, if options.mode==2, it generate surrogates by permuting
@@ -53,7 +53,7 @@ function [R2,pval,surrogates,c] = tudacv_sa(X,Y,T,cfg,Gamma)
 %                   temporal features are lost
 % Gamma: Precomputed decoding models time courses (optional)
 %
-% OUTPUT 
+% OUTPUT
 % R2: cross-validated explained variance
 % c: CV folds structure (c.training, c.test)
 % pval: if options.Nperm > 1 and options.mode==2, this is the pvalue for
@@ -69,42 +69,47 @@ mvafun    = str2fun(cfg.mva.method{1});
 N = length(T); q = size(Y,2); ttrial = T(1); p = size(X,2);
 responses = permute(reshape(Y,[ttrial N q]),[2 3 1]);
 responses = responses(:,:,1);
-pval = [];  
+pval = [];
 
 if isfield(cfg,'NCV')
     NCV = cfg.NCV; cfg = rmfield(cfg,'NCV');
-else, NCV = 10; 
+else
+    if isfield(cfg,'c')
+        NCV = length(cfg.c.training);
+    else
+        NCV = 10;
+    end
 end
-if isfield(cfg,'lambda') 
+if isfield(cfg,'lambda')
     lambda = cfg.lambda; cfg = rmfield(cfg,'lambda');
-else, lambda = 0.0001; 
+else, lambda = 0.0001;
 end
-if isfield(cfg,'lossfunc') 
+if isfield(cfg,'lossfunc')
     lossfunc = cfg.lossfunc; cfg = rmfield(cfg,'lossfunc');
-else, lossfunc = 'quadratic'; 
+else, lossfunc = 'quadratic';
 end
 if isfield(cfg,'Nfeatures') && cfg.Nfeatures>0 && cfg.Nfeatures<p
     warning('Specifying Nfeatures can lead to a biased calculation of CV-R2')
 end
-if isfield(cfg,'mode') 
+if isfield(cfg,'mode')
     mode = cfg.mode; cfg = rmfield(cfg,'mode');
-else, mode = 1; 
+else, mode = 1;
 end
-if isfield(cfg,'CVmethod') 
+if isfield(cfg,'CVmethod')
     CVmethod = cfg.conservative; cfg = rmfield(cfg,'CVmethod');
-else, CVmethod = 1; 
+else, CVmethod = 1;
 end
-if isfield(cfg,'Nperm') 
+if isfield(cfg,'Nperm')
     Nperm = cfg.Nperm; cfg = rmfield(cfg,'Nperm');
-else, Nperm = 1; 
+else, Nperm = 1;
 end
-if isfield(cfg,'verbose') 
+if isfield(cfg,'verbose')
     verbose = cfg.verbose; cfg = rmfield(cfg,'verbose');
-else, verbose = 1; 
+else, verbose = 1;
 end
 if nargin < 5, Gamma = []; end
 
-if ~all(T==T(1)), error('All elements of T must be equal'); end 
+if ~all(T==T(1)), error('All elements of T must be equal'); end
 
 % Form CV folds; if response are categorical, then it's stratified
 samples_per_value = length(Y) / length(unique(Y));
@@ -142,7 +147,7 @@ else
 end
 
 % Preproc data and put in the right format
-%[X,Y,T,cfg] = preproc4hmm(X,Y,T,cfg); 
+%[X,Y,T,cfg] = preproc4hmm(X,Y,T,cfg);
 cfg = remove_options(cfg);
 p = size(X,2); q = size(Y,2);
 X = reshape(X,[ttrial N p]);
@@ -151,11 +156,11 @@ RidgePen = lambda * eye(p);
 
 % Get state time courses
 if isempty(Gamma)
-    cfg.verbose = 0; 
+    cfg.verbose = 0;
     [~,Gamma] = tudatrain(reshape(X,[ttrial*N p]),...
         reshape(Y,[ttrial*N q]),T,cfg);
 end
-K = size(Gamma,2); 
+K = size(Gamma,2);
 Gamma = reshape(Gamma,[ttrial N K]);
 
 % Estimate testing state time courses if necessary
@@ -196,110 +201,35 @@ else
     Gammapred = Gamma;
 end
 
-if mode == 1 %  time point by time point CV (i.e. not across time points)
-    Ypred = zeros(ttrial,N,q,'single');
-    surrogates = zeros(ttrial,Nperm);
-    for r = 1:Nperm
-        if r==1
-            G = Gamma;
-            Gp = Gammapred;
-        else
-            pe = randperm(N,N);
-            G = Gamma(:,pe,:);
-            Gp = Gammapred(:,pe,:);
-        end
-        for icv = 1:NCV
-            Ntr = length(c.training{icv}); Nte = length(c.test{icv});
-            Xtrain = reshape(X(:,c.training{icv},:),[ttrial*Ntr p]);
-            ytrain = reshape(Y(:,c.training{icv},:),[ttrial*Ntr q]);
-            Xtest = reshape(X(:,c.test{icv},:),[ttrial*Nte p]);
-            Gammatrain = reshape(G(:,c.training{icv},:),[ttrial*Ntr K]);
-            Gammatest = reshape(Gp(:,c.test{icv},:),[ttrial*Nte K]);
-            for k = 1:K
-                sGamma = repmat(sqrt(Gammatrain(:,k)),1,p);
-                Xtrain_k = Xtrain .* sGamma;
-                %ytrain_k = ytrain .* sGamma(:,1:q);%% FIXME:valid to skip
-                %this ste;??
-                %Beta = (Xtrain_k' * Xtrain_k + RidgePen) \ (Xtrain_k' * ytrain_k);
-                [model{k},result]  = mvafun(cfg,Xtrain_k,Xtest,ytrain);
-                sGamma = repmat(Gammatest(:,k),[1 q]); 
-                Ypred(:,c.test{icv},:) = Ypred(:,c.test{icv},:) + ...
-                    reshape( result .* sGamma , [ttrial Nte q]);
-            end
-            model{icv}.weights = model;
-            stat.result{icv} = Ypred;
-        end
-        if verbose && mod(r,10)==0
-            disp(['Permutation ' num2str(r)])
-        end
-        surrogates(:,r) = get_R2(Y,Ypred,lossfunc);
-    end
-    R2 = surrogates(:,1);
-    if Nperm > 1 
-        pval = sum(repmat(R2,[1 Nperm]) <= surrogates ,2) / (Nperm+1);
-        surrogates = surrogates(:,2:end);
-    else
-        surrogates = [];
-    end
-    
-else % cross-time generalisation 
-    surrogates = zeros(ttrial,ttrial,Nperm); 
-    for r = 1:Nperm
-        if r==1
-            G = Gamma;
-            Gp = Gammapred; 
-        else
-            pe = randperm(N,N);
-            G = Gamma(:,pe,:);
-            Gp = Gammapred(:,pe,:);
-        end
-        for t1 = 1:ttrial
-            Ypred = zeros(ttrial,N,q);
-            for icv = 1:NCV % This computation could be done just once
-                % instead of t1=1:trial, but it would consume lots of memory
-                Ntr = sum(c.training{icv}); Nte = sum(c.test{icv});
-                Xtrain = reshape(X(:,c.training{icv},:),[ttrial*Ntr p]);
-                ytrain = reshape(Y(:,c.training{icv},:),[ttrial*Ntr q]);
-                Gammatrain = reshape(G(:,c.training{icv},:),[ttrial*Ntr K]);
-                Beta = zeros(p,q,K);
-                for k = 1:K
-                    % Beta is estimated using all time points in training trials
-                    sGamma = repmat(sqrt(Gammatrain(:,k)),1,p);
-                    Xtrain_k = Xtrain .* sGamma;
-                    ytrain_k = ytrain .* sGamma(:,1:q);
-                    Beta(:,:,k) = (Xtrain_k' * Xtrain_k + RidgePen) \ ...
-                        (Xtrain_k' * ytrain_k);
-                end
-                Gammatest = Gp(:,c.test{icv},:);
-                nn = find(c.test{icv}');
-                for j = 1:length(nn)
-                    n = nn(j);
-                    % Beta_t1_n is a weighted sum over states,
-                    % drawing from Gamma in testing segment, and time point t1
-                    Beta_t1_n = zeros(p,q);
-                    for k = 1:K
-                        Beta_t1_n = Beta_t1_n + Gammatest(t1,j,k) * Beta(:,:,k);
-                    end
-                    Xtest = permute(X(:,n,:),[1 3 2]); % ttrial by p
-                    Ypred(:,n,:) = Xtest * Beta_t1_n;
-                end
-            end
-            surrogates(t1,:,r) = get_R2(Y,Ypred,lossfunc);
-        end
-        if verbose && mod(r,10)==0
-            disp(['Permutation ' num2str(r)])
-        end
-    end
-    R2 = surrogates(:,:,1);
-    if Nperm > 1 
-        pval = sum(repmat(R2,[1 1 Nperm]) <= surrogates ,3) / (Nperm+1);
-        surrogates = surrogates(:,:,2:end);
-    else
-        surrogates = [];
-    end
-    
-end
+Ypred = zeros(ttrial,N,2,'single');%%FIXME: hard coded dimension
 
+G = Gamma;
+Gp = Gammapred;
+
+for icv = 1:NCV
+    Ntr = length(c.training{icv}); Nte = length(c.test{icv});
+    Xtrain = reshape(X(:,c.training{icv},:),[ttrial*Ntr p]);
+    ytrain = reshape(Y(:,c.training{icv},:),[ttrial*Ntr q]);
+    Xtest = reshape(X(:,c.test{icv},:),[ttrial*Nte p]);
+    Gammatrain = reshape(G(:,c.training{icv},:),[ttrial*Ntr K]);
+    Gammatest = reshape(Gp(:,c.test{icv},:),[ttrial*Nte K]);
+    for k = 1:K
+        sGamma = repmat(sqrt(Gammatrain(:,k)),1,p);
+        Xtrain_k = Xtrain .* sGamma;
+        %ytrain_k = ytrain .* sGamma(:,1:q);%% FIXME:valid to skip
+        %this ste;??
+        %Beta = (Xtrain_k' * Xtrain_k + RidgePen) \ (Xtrain_k' * ytrain_k);
+        [model{k},result]  = mvafun(cfg,Xtrain_k,Xtest,ytrain);
+        %sGamma = repmat(Gammatest(:,k),[1 q]);
+        [maxval,maxind] = max(Gammatest,[],2);
+        sGamma = (maxind==k);
+        Ypred(:,c.test{icv},:) = Ypred(:,c.test{icv},:) + reshape(result .* sGamma, [ttrial Nte size(result,2)]);
+    end
+    results{icv} = reshape(Ypred(:,c.test{icv},:),[ttrial*Nte size(result,2)]);
+    ytest = reshape(Y(:,c.test{icv},:),[ttrial*Nte q]);
+    design{icv} = ytest;
+    models{icv}.weights = model;
+end
 end
 
 
@@ -312,37 +242,7 @@ if isfield(options,'onpower'), options = rmfield(options,'onpower'); end
 if isfield(options,'standardise'), options = rmfield(options,'standardise'); end
 if isfield(options,'embeddedlags'), options = rmfield(options,'embeddedlags'); end
 if isfield(options,'pca'), options = rmfield(options,'pca'); end
-options.add_noise = 0; 
+options.add_noise = 0;
 end
 
 
-function R2 = get_R2(Y,Ypred,lossfunc)
-d = Y - Ypred;
-if strcmp(lossfunc,'quadratic')
-    l = d.^2; l0 = Y.^2; ee = 1/2;
-elseif strcmp(lossfunc,'absolute')
-    l = abs(d); l0 = abs(Y); ee = 1;
-elseif strcmp(lossfunc,'huber')
-    l = zeros(size(d)); l0 = zeros(size(d)); ee = 1;
-    for j1 = 1:N
-        for j2 = 1:q
-            ii = abs(d(:,j1,j2))<1; l(ii,j1,j2) = d(ii,j1,j2).^2;
-            ii = abs(d(:,j1,j2))>=1; l(ii,j1,j2) = abs(d(ii,j1,j2));
-            ii = abs(Y(:,j1,j2))<1; l0(ii,j1,j2) = Y(ii,j1,j2).^2;
-            ii = abs(Y(:,j1,j2))>=1; l0(ii,j1,j2) = abs(Y(ii,j1,j2));
-        end
-    end
-end
-% across-trial R2, using mean of euclidean distances in stimulus space
-m = mean(sum(l,3).^(ee),2);
-m0 = mean(sum(l0,3).^(ee),2);
-R2 = 1 - m ./ m0;
-% % mean of R2, one per trial - equivalent to the previous
-% m = sum(l,3).^(ee);
-% m0 = sum(l0,3).^(ee);
-% R2 = mean(1 - m ./ m0,2);
-% % computing SE with all trials and features at once
-% se = sum(sum(l,3),2).^(ee);
-% se0 = sum(sum(l0,3),2).^(ee);
-% R2 = 1 - se ./ se0;
-end
