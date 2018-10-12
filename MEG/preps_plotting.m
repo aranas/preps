e334cb5dd40da7ccbbf99ce841006d207368300c
Sophie Arana <arana.sophie@gmail.com>
2018-10-12 11:55:33 +0200

    change according to changed saved files

--- MEG/preps_plotting.m
+++ MEG/preps_plotting.m
@@ -1,34 +1,25 @@
 %% This script plots classification accuracy and saves figure as png
 
-%% Default parameters
-if ~exist('subj',           'var'), subj         = 'pilot-005';                               end
-if ~exist('root_dir',       'var'), root_dir     = '/project/3011210.01/MEG/';                end
-if ~exist('save_dir',       'var'), save_dir     = '/project/3011210.01/MEG/Classification';  end
-if ~exist('classes',        'var'), classes      = {'NN', 'VVFIN'};                             end
-if ~exist('classifier',     'var'), classifier   = 'preps_naivebayes';                        end
-if ~exist('folds',          'var'), folds        = 20;                                        end
-if ~exist('numfeat',        'var'), numfeat      = 250;                                       end
-if ~exist('do_plotacc',     'var'), do_plotacc   = false;                                     end
-if ~exist('do_plotgeneral', 'var'), do_plotgeneral   = false;                                 end
-if ~exist('suffix',         'var'), suffix       = '';                                        end
-if ~exist('datasuffix',     'var'), datasuffix   = '';                                        end
-pos = {'ART','NN','VVFIN','ADJA','APPR','NA','VA','Fill'};
-trigger = {[111,114,121,124,211,214,221,224], %Determiner
-    [112,115,122,125,212,215,222,225],        %Nouns
-    [113,123,213,223],                        %Verbs
-    [118,128,218,228],                        %Adjectives
-    [116,126,216,226],                        %Preposition
-    [219,229],                                %last word Noun attached
-    [119,129],                                %last word Verb attached
-    [30:39]};                                 %all words in filler sentences
+%% parameters
+%which plot to generate
+if ~exist('do_plotacc',     'var'), do_plotacc      = false;            end
+if ~exist('do_plotgeneral', 'var'), do_plotgeneral  = false;            end
+if ~exist('do_plotbl',      'var'), do_plotbl       = false;            end
+if ~exist('do_confusion',   'var'), do_confusion    = false;            end
+
+if ~exist('filename',       'var'), filename        = '';               end
+
+root_dir    = '/project/3011210.01/MEG';
+fileparts   = strsplit(filename,'_');
+mode        = fileparts{1};
+subj        = fileparts{2};
+save_file   = fullfile(root_dir,'figures',mode,strrep(strjoin(fileparts(2:end),'_'),'mat','png'));
+
 %% plot classification accuracy
 if do_plotacc
-filename = fullfile(save_dir, subj, sprintf('classacc_%s%s_%dfolds_%dfeats_%s%s',subj,datasuffix,folds,numfeat,horzcat(classes{:}),suffix));
-
 load(filename)
-load(strcat(filename,'_shuf'))
 
-time = cfg.timeinfo-0.05;
+time = cfg.timeinfo-0.05;%fixme:outdated
 time = round(time*1000);
 
 figure('units','normalized','outerposition',[0 0 1 1])
@@ -89,31 +80,56 @@ if do_plotgeneral
 end
 
 if do_plotbl
-load(filename)
-% compute confidence intervals   
-accbounds       = std(acc,[],2);              
-accshufbounds   = std(accshuf,[],2);   
-macc            = mean(acc,2);
-maccshuf        = mean(accshuf,2);
+    load(fullfile(root_dir,'Classification',subj,filename))
+    if strcmp(cfgcv.mva,'ridgeregression_sa')
+        [m,n] = size(stat);
+        acc = reshape(vertcat(stat{:}),[m n 4]);
+        acc = cell2mat(squeeze(acc(:,:,1)));
+        
+        accshuf = reshape(vertcat(statshuf{:}),[m n 4]);
+        accshuf = cell2mat(squeeze(accshuf(:,:,1)));
+    else
+        stat = cell2mat(stat);
+        stat = struct2cell(stat);
+        statshuf = cell2mat(statshuf);
+        statshuf = struct2cell(statshuf);
+        
+        confusion = squeeze(stat(2,:,:));
+        confusionshuf = squeeze(statshuf(2,:,:));
+        mconf = mean(reshape(cat(3,confusion{:}),[5 5 19 50]),4);
+        stdconf = std(reshape(cat(3,confusion{:}),[5 5 19 50]),[],4);
+        
+        acc = cell2mat(squeeze(stat(1,:,:)));
+        accshuf = cell2mat(squeeze(statshuf(1,:,:)));
+    end
+    % compute confidence intervals
+    accbounds       = std(acc,[],2);
+    accshufbounds   = std(accshuf,[],2);
+    macc            = mean(acc,2);
+    maccshuf        = mean(accshuf,2);
+    
+    figure('units','normalized','outerposition',[0 0 1 1])
+    hold on;
+    taxis = round(cfgcv.time(1:nearest(cfgcv.time,cfgcv.time(end)-cfgcv.twidth))*1000);
+    [hl, hp] = boundedline(taxis, [macc maccshuf], [accbounds accshufbounds],'alpha');
+    set(hl,'linewidth',3)
+    
+    xticklabels(cfg.cvtime)
+    xlabel(sprintf('time in ms (center of %d ms sliding window)',cfgcv.twidth*1000))
+    ylabel('classification accuracy')
+    ylim([0 1])
+    title(sprintf('classifier: %s - classes:%s - %s',classifier, horzcat(classes{:}),subj),'interpreter','none')
+    legend('multiclass','permuted classes')
+    set(legend,'Location','best')
+    set(gca,'FontSize',25)
+    set(gca,'LineWidth',4)
+    
+    export_fig(save_file,'-png');
+    clf;
+end
 
-figure('units','normalized','outerposition',[0 0 1 1])
-hold on;
-[hl, hp] = boundedline(round(cfgcv.timeinfo*1000), [macc maccshuf], [accbounds accshufbounds],'alpha');
-set(hl,'linewidth',3)
-
-xticklabels(time)
-xlabel(sprintf('time in ms (center of %d ms sliding window)',cfgcv.twidth*1000))
-ylabel('classification accuracy')
-ylim([0.3 1])
-title(sprintf('classifier: %s - classes:%s - %s',classifier, horzcat(classes{:}),subj),'interpreter','none')
-legend('multiclass','permuted classes')
-set(legend,'Location','best')
-set(gca,'FontSize',25)
-set(gca,'LineWidth',4)
-
-fname = fullfile(save_dir,'/Figures/',filename);
-export_fig(fname,'-png');
-clf;
+if do_confusion
+    
 end
 
 
