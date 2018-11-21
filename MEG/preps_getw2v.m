fb6e61ce91a18b7933ede1aa74ca1cfaadd8d5d4
Sophie Arana <arana.sophie@gmail.com>
2018-11-21 10:45:29 +0100

    ENH: if amount of clusters specified, w2v will get clustered turning it into a multi-class decoding problem

--- MEG/preps_getw2v.m
+++ MEG/preps_getw2v.m
@@ -13,6 +13,7 @@ for smp = 1:size(data.trial,2)
         w2v.feat(smp,:)     = stimuli(id).words(str2num(trig(end))).w2v;
         w2v.pos{smp}        = stimuli(id).words(str2num(trig(end))).pos{1};
         w2v.word{smp}       = stimuli(id).words(str2num(trig(end))).word{1};
+        w2v.id{smp}         = id;
         indsel(smp) = 1;
     end
 end
@@ -38,14 +39,23 @@ if ncluster>1
     [cidx, cmeans] = kmeans(embeddings,ncluster,'dist','sqeuclidean');
     % [silh,h] = silhouette(embeddings,cidx,'sqeuclidean');
     labels = cidx(IC);
-end
-%t-sne to visually inspect natural clustering 
-% rng default
-% Y3 = tsne(embeddings,cidx,3,50);
-% figure
-% scatter3(Y3(:,1),Y3(:,2),Y3(:,3),30,cidx,'filled');
-% %view(-93,14)
-% 
-% text(Y3(:,1),Y3(:,2),Y3(:,3),w2v.word,'Fontsize',10);
-
+    
+    %t-sne to visually inspect natural clustering
+    % rng default
+    % Y3 = tsne(embeddings,cidx,3,50);
+    % figure
+    % scatter3(Y3(:,1),Y3(:,2),Y3(:,3),30,cidx,'filled');
+    % %view(-93,14)
+    %
+    % text(Y3(:,1),Y3(:,2),Y3(:,3),w2v.word,'Fontsize',10);
+    %how many verb-noun pairs end up in the same cluster?
+    data.trialinfo(:,3) = cidx(IC);
+    for id = 1:200
+        indx = find(data.trialinfo(:,2)==id);
+        if length(indx) == 4
+            same(id) = data.trialinfo(indx(2),3) == data.trialinfo(indx(3),3);
+        else
+            id
+        end
+    end
 end
