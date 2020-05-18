38133889ce641a6daabb63413eb74265964bb79e
Sophie Arana <arana.sophie@gmail.com>
2020-05-18 09:47:54 +0200

    bring code up-to-date for final figures

--- MEG/config_correlation.m
+++ MEG/config_correlation.m
@@ -7,12 +7,13 @@ maincfg.mode = 'normal';
 maincfg.classifier = 'ridgeregression_sa';
 maincfg.seltrig = {'NN','VVFIN','ADJA'};
 maincfg.dow2v = 1;
-%maincfg.w2vdim = 5;
+maincfg.w2vdim = 5;
 maincfg.statfun = {'eval_correlation'};
 maincfg.lambda = 1;
 maincfg.toverlap = 0.8;
 maincfg.repeats = 50;
-
+%maincfg.dattype = 'lcmv';
+%maincfg.parcel_indx = 1; %specify for each job
 maincfg
 
-suffix = '_w2vdim300_eval2'
+suffix = '_eval2'
--- MEG/eval_correlation.m
+++ MEG/eval_correlation.m
@@ -4,7 +4,7 @@ function [rho_mean, rho_var] = eval_correlation(cfg,obj)
 %separately
 rho = zeros(obj.folds,size(obj.result{1},2));
 for fold = 1:obj.folds
-    rho(fold,:) = diag(corr(obj.result{fold},obj.design{fold}))';
+    rho(fold,:) = diag(preps_corr(obj.result{fold},obj.design{fold}))';
 end
 rho_var = var(rho);
 rho_mean= mean(rho);
