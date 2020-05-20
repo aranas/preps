f63981e2aa07f20d808a3828648d9d9b2389909a
Sophie Arana <arana.sophie@gmail.com>
2020-05-20 18:56:06 +0200

    implement RSA approach within parcel over time

--- MEG/preps_w2v_RSA.m
+++ MEG/preps_w2v_RSA.m
@@ -11,7 +11,7 @@
 clear all
 
 %flags
-do_source = false;
+do_source = true;
 maincfg.subj = 'sub-002';
 maincfg.datasuffix  = '_lp01';
 maincfg.seltrig     = [114,124,214,224,115,125,215,225,113,123,213,223];
@@ -58,6 +58,7 @@ for w = 1:length(datasel.trialinfo)
         wskip = [wskip w];
     end
 end
+clear stimuli
 %delete those trials (words) from data, for which we don't have word
 %embeddings
 cfg = [];
@@ -78,15 +79,49 @@ embd = embd(sort_id,:);
 w2v_RDM = squareform(pdist(zscore(embd')','euclidean'));
 figure;imagesc(w2v_RDM)
 
-%neural RDM per time point
+%neural RDM per parcel (across time)
 data = reshape(cell2mat(datasel.trial),[nparc, nt, nword]);
 data = data(:,:,sort_id);
 
 brain_RDM = zeros(nt,nword,nword);
-for t = 1:nt
-    tmp = zscore(squeeze(data(:,t,:)))';
-    brain_RDM(t,:,:) = squareform(pdist(tmp,'euclidean'));
+for p = 1:nparc
+    tmp = zscore(squeeze(data(p,:,:)))';
+    brain_RDM(p,:,:) = squareform(pdist(tmp,'euclidean'));
 end
-t = nearest(datasel.time{1},0.15);
-figure;imagesc(squeeze(brain_RDM(t,:,:)))
+clear data tmp
 
+%correlate
+n_trials = 196;
+rho = [];
+for p = 1:nparc
+    tmp_brain = squeeze(brain_RDM(p,401:end,401:end));
+    tmp_model = w2v_RDM(1:200,1:200);
+    rho(p) = corr(tmp_model(tril(true(n_trials,n_trials),-1)),tmp_brain(tril(true(n_trials,n_trials),-1)),'type','spearman','rows', 'complete');
+end
+
+%Visualize
+%map to full source space
+load atlas_subparc374_8k
+pindx = 1:length(atlas.parcellationlabel);
+pindx([1 2 188 189]) = []; %ignore medial wall parcels
+mapped_rsm = zeros(size(atlas.parcellationlabel));
+for p = 1:nparc
+    indx = pindx(p);
+    mapped_rho(indx) = rho(p);
+end
+
+% create the 'upsampling matrix', to map parcels onto the vertices of the
+% cortical sheet
+x = zeros(0,1);
+y = zeros(0,1);
+for k = 1:numel(atlas.parcellationlabel)
+    sel = atlas.parcellation==k;
+    x = cat(1,x(:),find(sel));
+    y = cat(1,y(:),ones(sum(sel),1)*k);
+end
+P = sparse(x,y,ones(numel(x),1),size(atlas.pos,1),numel(atlas.parcellationlabel));
+
+figure;ft_plot_mesh(atlas,'vertexcolor',P*mapped_rho');
+lighting gouraud; material dull;
+view([-90 0])
+camlight;
