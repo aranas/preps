1c4df14d2133e4328f24b2f042c57efa01b469e4
Sophie Arana <arana.sophie@gmail.com>
2020-05-29 14:24:04 +0200

    change w2v_RSA script so that multiple trial selection can be chosen as a model for RSA analysis

--- MEG/preps_rsa_correlate.m
+++ MEG/preps_rsa_correlate.m
@@ -13,7 +13,8 @@ function out = preps_rsa_correlate(data,distancemeasure,model,correlation,perm_i
 % model.rdm =  model RDMs
 % model.name = corresponding condition names
 % correlation used to correlate data and model RDMs (i.e. 'Spearman','Kendall')
-% perm_idx contains the permuted trial ordering for each permutation.
+% perm_idx contains the permuted trial ordering for each permutation. If
+% perm_idx is empty, no permutations will be computed
 n_trials = size(data,1);
 
 for t = 1:size(data,3) %for feature (voxel or time)
@@ -26,9 +27,11 @@ for t = 1:size(data,3) %for feature (voxel or time)
         out(m).name = model.name{m};
         out(m).rho(t) = corr(model.rdm{m}(tril(true(n_trials,n_trials),-1)),tmpdat(tril(true(n_trials,n_trials),-1)),'type',correlation,'rows', 'complete');
         
-        for r = 1:size(perm_idx,1) %for each permutation
-            tmpdat_r = tmpdat(perm_idx(r,:),perm_idx(r,:));
-            out(m).rhorand(t,r) = corr(model.rdm{m}(tril(true(n_trials,n_trials),-1)),tmpdat_r(tril(true(n_trials,n_trials),-1)),'type',correlation,'rows', 'complete');
+        if ~isempty(perm_idx)
+            for r = 1:size(perm_idx,1) %for each permutation
+                tmpdat_r = tmpdat(perm_idx(r,:),perm_idx(r,:));
+                out(m).rhorand(t,r) = corr(model.rdm{m}(tril(true(n_trials,n_trials),-1)),tmpdat_r(tril(true(n_trials,n_trials),-1)),'type',correlation,'rows', 'complete');
+            end
         end
     end
 end
--- MEG/preps_w2v_RSA.m
+++ MEG/preps_w2v_RSA.m
@@ -8,127 +8,133 @@ clear all
 
 %parameters
 do_source = true;
-maincfg.datasuffix  = '_lp01';
-maincfg.seltrig     = [112,115,122,125,212,215,222,225,113,123,213,223,118,128,218,228];
-toverlap = 0.5;
+datasuffix  = '_lp01';
+%seltrig contains the selection of trigger values for both brain data and
+%any amount of models to be tested, such that seltrig{1} = brain data and
+%seltrig{2:n} = triggers for model comparison.
+all_seltrig     = {[112,115,122,125,212,215,222,225,113,123,213,223,118,128,218,228]};
+toverlap = 0.2;
 twidth = 0.1;
-num_perm = 100;
-
-%compute permutations
-rng(5)
-perm_idx = zeros(num_perm,nword);
-for r = 1:num_perm
-    perm_idx(r,:) = randperm(nword);
-end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%% For each subject compute & save %%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%% correlation between model RDM and neural RDM %%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
+jobid = {};
 for nsub = 1:10
-    maincfg.subj = sprintf('sub-%.3d',nsub);%map to full source space
+    subj = sprintf('sub-%.3d',nsub);%map to full source space
     
     %% Load/select data & embeddings
-    %get filenames & trigger
-    root_dir    = '/project/3011210.01/MEG';
-    channelfile     = fullfile(root_dir,sprintf('%s_dataclean%s.mat',maincfg.subj, maincfg.datasuffix));
-    lcmvfile        = fullfile(root_dir,strcat(maincfg.subj,'_preps_lcmv_parc.mat'));
-    [seltrig, pos] = preps_help_collecttrig(maincfg.subj, maincfg.seltrig);
     
-    %load channel level data
+    root_dir    = '/project/3011210.01/MEG';
+    channelfile     = fullfile(root_dir,sprintf('%s_dataclean%s.mat',subj, datasuffix));
+    lcmvfile        = fullfile(root_dir,strcat(subj,'_preps_lcmv_parc.mat'));
     load(channelfile,'data')
-    sel = ones(length(data.trialinfo),1);
-    sel             = sel & ismember(data.trialinfo(:,1),seltrig);
-    cfg             = [];
-    cfg.trials      = sel;
-    datasel         = ft_selectdata(cfg,data);
+    
+    for i = 1:length(all_seltrig)
+        
+        [seltrig{i}, pos{i}] = preps_help_collecttrig(subj, all_seltrig{i});
+        sel = ones(length(data.trialinfo),1);
+        sel             = sel & ismember(data.trialinfo(:,1),seltrig{i});
+        cfg             = [];
+        cfg.trials      = sel;
+        datasel{i}         = ft_selectdata(cfg,data);
+        
+    end
     clear data
+    upos = uniqueStrCell(pos{1});
+    suffix = strcat(upos{:});
     
     %convert to source data
     if do_source
-        maincfg.parcel_indx = [];
+        parcel_indx = [];
         num_comp = 1;
         load(lcmvfile);
         source_parc.filterlabel = filterlabel;
-        datasel = preps_sensor2parcel(datasel,source_parc,num_comp,maincfg.parcel_indx);
+        for i = 1:length(datasel)
+            datasel{i} = preps_sensor2parcel(datasel{i},source_parc,num_comp,parcel_indx);
+        end
         clear source_parc source filterlabel
     end
     
     %select word embeddings corresponding to selected trials
     load preps_stimuli
     embd = [];
-    pos = [];
+    PoS = [];
+    wids = [];
     words = {};
-    wrd2trl = cell(length(datasel.trialinfo),1);
-    for w = 1:length(datasel.trialinfo)
-        wid = datasel.trialinfo(w,2);
-        wnum = num2str(datasel.trialinfo(w,1));
-        wnum = str2double(wnum(3));
-        if ~isempty(stimuli(wid).words(wnum).w2v)
-            if ~any(ismember(words,stimuli(wid).words(wnum).lemma))
-                words = [words stimuli(wid).words(wnum).lemma];
-                embd = [embd; stimuli(wid).words(wnum).w2v];
-                pos = [pos wnum];
+    wrd2trl = cell(length(datasel{end}.trialinfo),1);
+    for i = 1:max(length(datasel)-1,1)
+        for w = 1:length(datasel{i}.trialinfo)
+            wid = datasel{i}.trialinfo(w,2);
+            wnum = num2str(datasel{i}.trialinfo(w,1));
+            wnum = str2double(wnum(3));
+            if ~isempty(stimuli(wid).words(wnum).w2v)
+                if ~any(ismember(words,stimuli(wid).words(wnum).lemma))
+                    words = [words stimuli(wid).words(wnum).lemma];
+                    embd = [embd; stimuli(wid).words(wnum).w2v];
+                    PoS = [PoS wnum];
+                    wids = [wids wid];
+                end
+                id = find(ismember(words,stimuli(wid).words(wnum).lemma));
+                wrd2trl{id} = [wrd2trl{id} w];
             end
-            id = find(ismember(words,stimuli(wid).words(wnum).lemma));
-            wrd2trl{id} = [wrd2trl{id} w];
         end
+        all_pos{i} = PoS;
+        all_wids{i} = wids;
+        
+        %model RDM based on embeddings
+        model_RDM = squareform(pdist(zscore(embd')','euclidean'));
+        %figure;imagesc(model_RDM)
+        model.rdm{i} = model_RDM;
+        upos = uniqueStrCell(pos{i});
+        model.name{i} = strcat(upos{:});
     end
-    clear stimuli
-    nword = length(words);
-    [nparc, nt] = size(datasel.trial{1});
+    clear stimuli model_RDM
     
+    nword = length(words);
+    [nparc, nt] = size(datasel{end}.trial{1});
     % Average over repeated words
     brain_perword = zeros(nword,nparc,nt);
     for w = 1:length(words)
-        trlsel = datasel.trial(wrd2trl{w});
+        trlsel = datasel{end}.trial(wrd2trl{w});
         brain_perword(w,:,:) = mean(cat(3,trlsel{:}),3);
     end
-    
-    %% Create similarity matrices for data & model and Correlate
-    
-    %sort for part of speech
-    [n,sort_id] = sort(pos);
-    embd = embd(sort_id,:);
-    brain_perword = brain_perword(sort_id,:,:);
-    
-    %model RDM based on embeddings
-    model_RDM = squareform(pdist(zscore(embd')','euclidean'));
-    figure;imagesc(model_RDM)
-    model.rdm{1} = model_RDM;
-    model.name{1} = 'contentwords_embeddings';
-    
+    clear trlsel
+    %% Create similarity matrices for data and Correlate with models  
     %neural RDM per parcel (searchlight across time) & correlate
-    begtim = datasel.time{1}(1);
-    endtim = datasel.time{1}(end);
+    begtim = datasel{end}.time{1}(1);
+    endtim = datasel{end}.time{1}(end);
     endtim = endtim - twidth;
     time = linspace(begtim, endtim, round(abs(begtim-endtim) ./ ...
         (twidth - toverlap * twidth)) + 1);
     
-    jobid = {};
     for t = 1:length(time)
-        begtim = nearest(datasel.time{1},time(t));
-        endtim = nearest(datasel.time{1},time(t)+twidth);
+        begtim = nearest(datasel{end}.time{1},time(t));
+        endtim = nearest(datasel{end}.time{1},time(t)+twidth);
         data = permute(brain_perword(:,:,begtim:endtim),[1,3,2]);
         
-        jobid{t} = qsubfeval('preps_rsa_correlate',data,'euclidean',model,'spearman',perm_idx,1,...
-            'memreq',(1024^3)*5,'timreq',60*60,'batchid',sprintf('preps_rsa_t%i_%s',t,maincfg.subj));
+        jobid{nsub,t} = qsubfeval('preps_rsa_correlate',data,'euclidean',model,'spearman',[],1,...
+            'memreq',(1024^3)*5,'timreq',60*30,'batchid',sprintf('preps_rsa_t%i_%s_%s',t,subj,suffix));
     end
-    rho = zeros(length(time),nparc);
-    rhorand = zeros(length(time),nparc,num_perm);
+    allinfo{nsub}.pos = all_pos;
+    allinfo{nsub}.wid = all_wids;
+    allinfo{nsub}.model = model;
+end
+%Collect results
+for nsub = 1:10
+    subj = sprintf('sub-%.3d',nsub);%map to full source space
+ 
+    rho = [];
     for t = 1:length(time)
-        try
-            out = qsubget(jobid{t});
-        catch
-            load([jobid{t} '_output.mat'])
-            out = argout{1};
+        load([jobid{nsub,t} '_output.mat'])
+        out = argout{1};            
+        for i = 1:length(out)
+            rho(i,t,:) = out(i).rho;
         end
-        rho(t,:) = out.rho;
-        rhorand(t,:,:) = out.rhorand;
     end
-    
-    save(fullfile(root_dir,'RSA','w2v',sprintf('%s_contentwords_rho.mat',maincfg.subj)),'rho','rhorand')
+    modelinfo = allinfo{nsub};
+    save(fullfile(root_dir,'RSA','w2v',sprintf('%s_%s_rho.mat',subj,suffix)),'rho','modelinfo','time')
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -137,71 +143,71 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Collect results
+clear all
 root_dir    = '/project/3011210.01/MEG';
 load atlas_subparc374_8k
-data = load(fullfile(root_dir,'sub-001_dataclean_lp01.mat'));
 
-begtim = data.data.time{1}(1);
-endtim = data.data.time{1}(end);
-endtim = endtim - twidth;
-time = linspace(begtim, endtim, round(abs(begtim-endtim) ./ ...
-    (twidth - toverlap * twidth)) + 1);
-
-mapped_rho = zeros(10,size(atlas.parcellationlabel,1),length(time));
-mapped_rhorand = zeros(10,size(atlas.parcellationlabel,1),length(time),num_perm);
+mapped_rho = [];
 for nsub = 1:10
-    maincfg.subj = sprintf('sub-%.3d',nsub);%map to full source space
-    load(fullfile(root_dir,'RSA','w2v',sprintf('%s_contentwords_rho.mat',maincfg.subj)))
+    subj = sprintf('sub-%.3d',nsub);%map to full source space
+    load(fullfile(root_dir,'RSA','w2v',sprintf('%s_ADJANNVVFIN_rho.mat',subj)))
     
     pindx = 1:length(atlas.parcellationlabel);
     pindx([1 2 188 189]) = []; %ignore medial wall parcels
-    for p = 1:size(rho,2)
-        indx = pindx(p);
-        mapped_rho(nsub,indx,:) = rho(:,p)';
-        mapped_rhorand(nsub,indx,:,:) = permute(rhorand(:,p,:),[2,1,3]);
+    for i = 1:size(rho,1) %for every model
+        for p = 1:size(rho,3)
+            indx = pindx(p);
+            mapped_rho(i,nsub,indx,:) = squeeze(rho(i,:,p));
+        end
     end
 end
-all_rho = squeeze(mean(mapped_rho));
-all_rhorand = squeeze(mean(mapped_rhorand));
+for i = 1:size(mapped_rho,1)
+avg_rho{i} = squeeze(mean(squeeze(mapped_rho(1,:,:,:))));
+end
+
+mapped_rho(:,:,[1 2 188 189],:) = nan;
 %% Stats
-
-cfg                  = [];
-cfg.connectivity     = parcellation2connmat(atlas);
-cfg.tail             = 1;
-cfg.clustertail      = 1;
-cfg.clusterthreshold = 'nonparametric_individual';
-cfg.clusteralpha     = 0.05;
-cfg.feedback         = 'text';
-cfg.clusterstatistic = 'maxsum';
-
-cfg.dim = size(all_rhorand(:,:,1));
-cfg.numrandomization = size(all_rhorand,3);
-
-all_rho(all_rho==0) = nan;
-all_rhorand(all_rhorand==0) = nan;
-statobs  = reshape(all_rho,[],1);
-statrand = reshape(all_rhorand,[],size(all_rhorand,3));
-
-stats = clusterstat(cfg, statrand, statobs);
-fn = fieldnames(stats);
-for k = 1:numel(fn)
-    try
-        stats.(fn{k}) = reshape(stats.(fn{k}),cfg.dim);
-    end
-end
-
-
-%% Visualize
-load(fullfile('/project/3011210.01/anatomy',maincfg.subj,strcat(maincfg.subj,'_sourcemodel.mat')));
+Nsub = 10;
+modeli = 1;
+load(fullfile('/project/3011210.01/anatomy',subj,strcat(subj,'_sourcemodel.mat')));
 source                = [];
 source.brainordinate  = atlas;
 source.brainordinate.pos = sourcemodel.pos;
 source.label          = atlas.parcellationlabel;
 source.time           = time;
-source.dimord         = 'chan_time';
-source.pow            = all_rho;%.*(stats.prob<0.05);
+%source.dimord         = 'chan_time';
 
+
+for nsub = 1:Nsub
+    all_sources{nsub} = source;
+    all_sources{nsub}.pow = squeeze(mapped_rho(modeli,nsub,:,:));
+    all_bsl{nsub} = source;
+    all_bsl{nsub}.pow = zeros(size(all_sources{nsub}.pow));
+end
+
+cfg = [];
+cfg.method = 'montecarlo';
+cfg.statistic = 'ft_statfun_depsamplesT';
+cfg.parameter = 'pow';
+cfg.correctm = 'cluster';
+cfg.inside = true(1,length(atlas.parcellationlabel));
+cfg.inside([1 2 188 189]) = false;
+cfg.neighbours = 'biosemi16_neighb.mat'; %dummy neighbourhood structure
+cfg.connectivity     = parcellation2connmat(atlas);
+cfg.alpha = 0.05;
+cfg.tail = 1;
+cfg.design(1,1:2*Nsub) = [ones(1,Nsub) 2*ones(1,Nsub)];
+cfg.design(2,1:2*Nsub) = [1:Nsub 1:Nsub];
+cfg.numrandomization = 1000;
+cfg.ivar = 1;
+cfg.uvar = 2;
+stat = ft_timelockstatistics(cfg,all_sources{:},all_bsl{:});
+
+
+%% Visualize
 %for exploration
+source.pow = avg_rho{modeli}.*stat.mask; 
+
 cfgp                  = [];
 cfgp.funparameter     = 'pow';
 %cfgp.maskparameter    = source.mask;
@@ -210,16 +216,17 @@ ft_sourcemovie(cfgp, source);
 
 % create the 'upsampling matrix', to map parcels onto the vertices of the
 % cortical sheet
-% x = zeros(0,1);
-% y = zeros(0,1);
-% for k = 1:numel(atlas.parcellationlabel)
-%     sel = atlas.parcellation==k;
-%     x = cat(1,x(:),find(sel));
-%     y = cat(1,y(:),ones(sum(sel),1)*k);
-% end
-% P = sparse(x,y,ones(numel(x),1),size(atlas.pos,1),numel(atlas.parcellationlabel));
-% 
-% figure;ft_plot_mesh(atlas,'vertexcolor',P*mapped_rho');
-% lighting gouraud; material dull;
-% view([-90 0])
-% camlight;
+x = zeros(0,1);
+y = zeros(0,1);
+for k = 1:numel(atlas.parcellationlabel)
+    sel = atlas.parcellation==k;
+    x = cat(1,x(:),find(sel));
+    y = cat(1,y(:),ones(sum(sel),1)*k);
+end
+P = sparse(x,y,ones(numel(x),1),size(atlas.pos,1),numel(atlas.parcellationlabel));
+
+
+%figure;ft_plot_mesh(atlas,'vertexcolor',P*squeeze(mapped_rho');
+lighting gouraud; material dull;
+view([-90 0])
+camlight;
